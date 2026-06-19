3403d8f9043d0cea03c72c7d4e3980409f678202
tasneemakhaled <khaledtasneem402@gmail.com>
2026-06-19 10:59:42 +0300

    create app images

---
+++ lib/core/utils/app_images.dart
@@ -0,0 +1,8 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+
+  /// Assets for assetsImagesLogo
+  /// assets/images/logo.svg
+  static const String assetsImagesLogo = "assets/images/logo.svg";
+}
