1b81eee8e4f96bd069f01c723f1a12037a0210b0
tasneemakhaled <khaledtasneem402@gmail.com>
2026-07-11 18:19:37 +0300

    create onboarding page view

---
+++ lib/features/onboarding/presentation/views/widgets/onboarding_page_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OnboardingPageView extends StatelessWidget {
+  const OnboardingPageView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
