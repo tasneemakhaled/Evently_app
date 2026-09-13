5b79cec0a9342a1f79e25b280600170e3555ac5f
tasneemakhaled <khaledtasneem402@gmail.com>
2026-09-13 08:32:09 +0300

    architect home view

---
+++ lib/features/home/presentation/views/widgets/custom_home_nav_bar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CustomHomeNavBar extends StatelessWidget {
+  const CustomHomeNavBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
