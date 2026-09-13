86e804c530165e5f0b04e24accddd801a8d307b2
tasneemakhaled <khaledtasneem402@gmail.com>
2026-09-13 08:45:42 +0300

    create home header

---
+++ lib/features/home/presentation/views/widgets/home_header.dart
@@ -0,0 +1,11 @@
+import 'package:evently_app/core/utils/constants.dart';
+import 'package:flutter/material.dart';
+
+class HomeHeader extends StatelessWidget {
+  const HomeHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(decoration: BoxDecoration(color: primaryColor));
+  }
+}
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:evently_app/features/home/presentation/views/widgets/home_header.dart';
 import 'package:flutter/material.dart';
 
 class HomeViewBody extends StatelessWidget {
@@ -5,6 +6,6 @@ class HomeViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(children: [HomeHeader()]);
   }
 }
