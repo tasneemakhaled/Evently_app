4363783b4f27f51fc5eb43d779ecf35a132658ee
tasneemakhaled <khaledtasneem402@gmail.com>
2026-09-15 16:04:18 +0300

    create bottom nav bar

--- lib/features/home/presentation/views/widgets/custom_home_nav_bar.dart
+++ lib/features/home/presentation/views/widgets/custom_home_nav_bar.dart
@@ -5,6 +5,6 @@ class CustomHomeNavBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return BottomNavigationBar(items: []);
   }
 }
