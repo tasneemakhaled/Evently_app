a4ed990da1ee34a74cdcdfae289ee1aa276827e7
tasneemakhaled <khaledtasneem402@gmail.com>
2026-09-18 11:07:14 +0300

    tab bar done

--- lib/features/home/presentation/views/widgets/custom_tab_bar.dart
+++ lib/features/home/presentation/views/widgets/custom_tab_bar.dart
@@ -1,5 +1,4 @@
 import 'package:evently_app/core/utils/app_images.dart';
-import 'package:evently_app/core/utils/app_text_styles.dart';
 import 'package:evently_app/core/utils/constants.dart';
 import 'package:flutter/material.dart';
 
@@ -9,6 +8,8 @@ class CustomTabBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return TabBar(
+      labelPadding: EdgeInsets.symmetric(horizontal: 8),
+      indicatorPadding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
       labelColor: primaryColor,
       unselectedLabelColor: Colors.white,
       tabAlignment: TabAlignment.start,
@@ -21,47 +22,75 @@ class CustomTabBar extends StatelessWidget {
       isScrollable: true,
       tabs: [
         Tab(
-          child: Row(
-            children: [
-              ImageIcon(AssetImage(Assets.assetsImagesCompassFilled)),
-              Text(
-                '  All',
-                // style: AppTextStyles.font16Medium.copyWith(color: Colors.white),
-              ),
-            ],
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 6, vertical: 6),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.white),
+              borderRadius: BorderRadius.circular(24),
+            ),
+            child: Row(
+              children: [
+                ImageIcon(AssetImage(Assets.assetsImagesCompassFilled)),
+                Text(
+                  '  All',
+                  // style: AppTextStyles.font16Medium.copyWith(color: Colors.white),
+                ),
+              ],
+            ),
           ),
         ),
         Tab(
-          child: Row(
-            children: [
-              ImageIcon(AssetImage(Assets.assetsImagesBikeFilled)),
-              Text(
-                '  Sport',
-                // style: AppTextStyles.font16Medium.copyWith(color: Colors.white),
-              ),
-            ],
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 6, vertical: 6),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.white),
+              borderRadius: BorderRadius.circular(24),
+            ),
+            child: Row(
+              children: [
+                ImageIcon(AssetImage(Assets.assetsImagesBikeFilled)),
+                Text(
+                  '  Sport',
+                  // style: AppTextStyles.font16Medium.copyWith(color: Colors.white),
+                ),
+              ],
+            ),
           ),
         ),
         Tab(
-          child: Row(
-            children: [
-              ImageIcon(AssetImage(Assets.assetsImagesBirthdayFilled)),
-              Text(
-                '  Birthday',
-                // style: AppTextStyles.font16Medium.copyWith(color: Colors.white),
-              ),
-            ],
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 6, vertical: 6),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.white),
+              borderRadius: BorderRadius.circular(24),
+            ),
+            child: Row(
+              children: [
+                ImageIcon(AssetImage(Assets.assetsImagesBirthdayFilled)),
+                Text(
+                  '  Birthday',
+                  // style: AppTextStyles.font16Medium.copyWith(color: Colors.white),
+                ),
+              ],
+            ),
           ),
         ),
         Tab(
-          child: Row(
-            children: [
-              ImageIcon(AssetImage(Assets.assetsImagesBookOpenFilled)),
-              Text(
-                '  Book Club',
-                // style: AppTextStyles.font16Medium.copyWith(color: Colors.white),
-              ),
-            ],
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 6, vertical: 6),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.white),
+              borderRadius: BorderRadius.circular(24),
+            ),
+            child: Row(
+              children: [
+                ImageIcon(AssetImage(Assets.assetsImagesBookOpenFilled)),
+                Text(
+                  '  Book Club',
+                  // style: AppTextStyles.font16Medium.copyWith(color: Colors.white),
+                ),
+              ],
+            ),
           ),
         ),
       ],
